b6f33f99bee05d60a0af8c52feec4b5dcbf29edd
joshwilkinson60@hotmail.co.uk <joshwilkinson60@hotmail.co.uk>
2020-09-07 15:33:05 +0000

    Skip and limit for user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Errors;
 use App\User;
 use Illuminate\Routing\Controller as BaseController;
 
 class UserController extends BaseController
 {
+  const ERROR_MAX_LIMIT_MESSAGE = 'Limit cannot be more than 50';
   public function get()
   {
-    return User::get();
+    $limit = request('limit', 10);
+    $offset = request('offset', 0);
+    if((int) $limit > 50){
+      return response()
+        ->json([
+            'message'   =>  Errors::ERROR_MAX_LIMIT_MESSAGE
+        ], 500);
+    }
+    return User::limit($limit)->offset($offset)->get();
   }
 }
