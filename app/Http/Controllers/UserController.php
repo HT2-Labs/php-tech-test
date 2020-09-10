67e62430bfee3975d5c9965f671387e83e33f77a
joshwilkinson60@hotmail.co.uk <joshwilkinson60@hotmail.co.uk>
2020-09-10 07:50:04 +0000

    adrian c attempt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Errors;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class UserController extends BaseController
 {
-  public function get()
+  public function get(Request $request)
   {
-    // $users = User::get();
-    return response('', 204);
+    $limit = $request->input('limit') ?? 10;
+    $offset = $request->input('offset') ?? 0;
+
+    if($limit > 50) {
+      return response([
+        'message' => Errors::ERROR_MAX_LIMIT_MESSAGE
+      ], 500);
+    }
+
+    $users = User::offset($offset)->limit($limit)->get();
+
+    return response($users, 200);
   }
 }
