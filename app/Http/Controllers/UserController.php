260b2c0bf7618364e27e6671c93ebbf64f78a624
joshwilkinson60@hotmail.co.uk <joshwilkinson60@hotmail.co.uk>
2020-09-10 09:07:53 +0000

    attempt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,47 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends BaseController
 {
-  public function get()
+  public function get($limit, $offset)
   {
     // $users = User::get();
-    return response('', 204);
+    if (($limit != '') && ($offset != '')) {
+      $users = User::paginate(10);
+    }
+    if ($limit <=200) {
+      if ($limit > 0  && $offset == '') {
+        $users = User::paginate($limit);
+      }
+
+      if ($limit > 0 && )
+    }
+
+
+
+    return response ($users, 200);
+  }
+  /**
+   * GetUserLimitsUsers with pagination
+   *
+   * @param int $limit
+   * @return void
+   */
+  public function getUserLimitsUsers($limit)
+  {
+    if ($limit < 200) {
+
+      return response ($users, 200);
+    }
+    return response ('error ', 500);
+  }
+
+  public function getUserLimitsWithOffsetUsers($limit, $offset)
+  {
+    $userObj = new User();
+    $users = $userObj->skip($offset)->take($limit)->get();
+    return response ($users, 200);
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', 'UserController@get')->name('get.users');
+Route::get('/users/limit/{limit}/offset/{offset}', 'UserController@get')->name('get.users');
